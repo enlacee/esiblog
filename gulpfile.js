18f12ad320357c4e0103981bfe6c098637175b4f
Anibal Copitan <anibal.copitan@altimea.com>
2016-06-15 17:16:10 -0500

    update

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ var jshint = require('gulp-jshint');
 var sass = require('gulp-sass');
 var cssmin = require('gulp-cssmin');
 var browserSync = require('browser-sync').create();
+var cssbeautify = require('gulp-cssbeautify');
 
 var config = {
 	baseDir: "./assets",
@@ -15,6 +16,10 @@ gulp.task('css', function(){
 			includePaths: [config.bowerDir + '/css'],
 		}))
 		// .pipe(cssmin())
+		.pipe(cssbeautify({
+			indent: '	',
+			openbrace: 'separate-line',
+			autosemicolon: true}))
 		.pipe(gulp.dest(config.baseDir + "/css"));
 });
 
